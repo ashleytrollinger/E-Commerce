4505ffdb516742b7ae1992e5e61d073184111dcd
ashleytrollinger <ashley@trollingers.com>
2023-06-11 18:06:00 -0400

    Working on the routes now

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,15 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const catagoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(catagoryData);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catagoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    res.status(200).json(catagoryData);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
