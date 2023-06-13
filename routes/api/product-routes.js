43f077c9e23be4717af8a04a66aa83027ea89f57
ashleytrollinger <ashley@trollingers.com>
2023-06-12 23:32:05 -0400

    trying to fix product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({
+    const productData = await Product.findAll(req.params.id, {
       include: [
         { model: Category },
         { model: Tag, through: ProductTag, as: 'product_tag' }
